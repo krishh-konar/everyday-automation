58e3717eb349afedbc9c477e7d7b9c5eb16dfbc0
Krishanu Konar <krishanukonar@gmail.com>
2024-09-13 22:50:35 +0100

    added cli argparse

--- ipo-alert/alert.py
+++ ipo-alert/alert.py
@@ -4,27 +4,43 @@ from bs4 import BeautifulSoup
 
 GMP_BASE_URL = "https://www.investorgain.com/report/live-ipo-gmp/331/"
 
-
 def cli() -> ArgumentParser:
-    """
-        Bootstrap CLI
-    """
-    parser = ArgumentParser(description="Extract IPO data from the given URL.")
-    parser.add_argument('url', type=str, help="The URL of the page to scrape.")
-    parser.add_argument('days_before_deadline', type=int, help="Number of days before the deadline.")
-    parser.add_argument('gmp_threshold', type=float, help="GMP threshold value.")
-    
+    """Bootstrap CLI for the script
+
+    Returns:
+        ArgumentParser: CLI arguments
+    """ 
+    parser = ArgumentParser(
+        description="Send IPO application alerts to configured endpoints."
+    )
+    parser.add_argument(
+        "-d",
+        "--days-before-close",
+        type=int,
+        help="Number of days before the IPO application closes.",
+        default=2,
+    )
+    parser.add_argument(
+        "-t",
+        "--alert-threshold",
+        type=float,
+        default=20.0,
+        help="GMP threshold value; percentage above which to return IPOs.",
+    )
+
     return parser.parse_args()
 
 
-def fetch_ipo_data():
-    """
-        Call IPO GMP page and parse IPO related data.
-    """
+def fetch_ipo_data() -> dict:
+    """Call IPO GMP page and parse IPO related data.
+
+    Returns:
+        dict: IPO data
+    """    
     response = get_url(GMP_BASE_URL)
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
 
         ipo_data = []
 
@@ -35,26 +51,26 @@ def fetch_ipo_data():
 
         # Check if the number of IPO, Est Listing, and Close elements match, assertion should be true.
         if len(ipo_elements) == len(est_listing_elements) == len(close_elements):
-            for ipo_element, est_listing_element, close_element in zip(ipo_elements, est_listing_elements, close_elements):
+            for ipo_element, est_listing_element, close_element in zip(
+                ipo_elements, est_listing_elements, close_elements
+            ):
                 # Find the <a> tag with target="_parent" within each IPO element
-                a_tag = ipo_element.find('a', attrs={"target": "_parent"})
+                a_tag = ipo_element.find("a", attrs={"target": "_parent"})
                 if a_tag:
-                    # Remove all <span> tags from the <a> tag's contents
-                    for span in a_tag.find_all('span'):
+                    # Remove all <span> tags from the <a> tag's contents, this contains additional listing data
+                    for span in a_tag.find_all("span"):
                         span.decompose()
                     ipo_value = a_tag.get_text(strip=True)
                 else:
                     ipo_value = None
 
-                # Extract text for Est Listing and Close, ignoring <span> tags
                 est_listing_value = est_listing_element.get_text(strip=True)
                 close_value = close_element.get_text(strip=True)
 
-                # Create a dictionary with the extracted values
                 entry = {
                     "IPO": ipo_value,
                     "Est_listing": est_listing_value,
-                    "Close": close_value
+                    "Close": close_value,
                 }
                 ipo_data.append(entry)
 
@@ -65,10 +81,29 @@ def fetch_ipo_data():
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
+    return ipo_data
+
+
+def filter_data(cli_args: ArgumentParser, ipo_data: dict) -> dict:
+    """
+    Filter out IPOs matching the filtering criteria provided via CLI
+
+    Args:
+        cli_args (ArgumentParser): CLI args
+        ipo_data (dict): All available IPOs data 
+
+    Returns:
+        dict: filtered IPOs
+    """    
+    filtered_list = {}
+
+    return filter_data
+
 
 def main():
     args = cli()
     print(args)
+    # ipo_data = fetch_ipo_data()
 
 
 if __name__ == "__main__":
